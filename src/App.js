59e91bbe58e0a16bcce42738078bf00c895d9871
aroyehunahmad <ahmadadewale800@gmail.com>
2021-08-30 15:28:41 +0100

    Ex. 6.13: When the application launches, fetch anecdotes from the backend implemented using json-server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
@@ -6,12 +6,16 @@ import Filter from './components/Filter'
 import Notification from './components/Notification'
 import { upvote, newAnecdote } from './reducers/anecdoteReducer'
 import { addNotification, removeNotification } from './reducers/notificationReducer'
+import { getAll } from './services/anecdotes'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    getAll().then(anecdotes => dispatch({type: 'INIT_ANECDOTES', data: anecdotes}))
+  }, [dispatch])
   const anecdotes = useSelector(state => state.anecdotes)
   const filteredAnecdotes = useSelector(state => state.filtered)
   console.log('filtered', filteredAnecdotes)
-  const dispatch = useDispatch()
   // const [filtered, setFiltered] = useState([])
 
   const vote = (id, content) => {
